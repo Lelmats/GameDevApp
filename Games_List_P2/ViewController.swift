da80512b3c30923b11dde57b6c486cab8dcaf5fa
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-09-29 08:15:15 -0700

    Initial Commit

---
+++ Games_List_P2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Games_List_P2
+//
+//  Created by Alumno on 9/29/22.
+//  Copyright © 2022 MaxRivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
